c2a73d6180aeaaf58f8a3a475803ea7b00317d67
Swopeparkmitch6211@gmail.com <191430198+StoneyLee63@users.noreply.github.com.>
2026-06-03 14:35:00 -0500

    Phase 0: Project initialization

---
+++ EchoProbe/src/echoprobe.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+usage() {
+    echo "Usage: ./echoprobe.sh"
+}
+
+main() {
+    echo "[*] EchoProbe starting..."
+}
+
+main "$@"
---
+++ TestProject/src/testproject.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+usage() {
+    echo "Usage: ./testproject.sh"
+}
+
+main() {
+    echo "[*] TestProject starting..."
+}
+
+main "$@"
